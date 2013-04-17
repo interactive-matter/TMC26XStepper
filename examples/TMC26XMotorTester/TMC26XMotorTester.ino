129a356e64755335d34a985469130478aa0faf72
Markus Klink <justjoheinz@gmail.com>
2013-04-17 11:23:17 +0200

    fix for ENABLE pin

--- examples/TMC26XMotorTester/TMC26XMotorTester.ino
+++ examples/TMC26XMotorTester/TMC26XMotorTester.ino
@@ -26,10 +26,13 @@
 #include <TMC26XStepper.h>
 
 //you may adapt this to your shield or breakout board connection
-#define CS_PIN 2
-#define DIR_PIN 6
-#define STEP_PIN 7
-#define ENABLE_PIN 8 //if it is not connected it won't be a problem
+#define CS_PIN 10
+#define DIR_PIN 7
+#define STEP_PIN 4
+//if it is not connected it won't be a problem
+// Comment the line if you do not have an enable pin assigned, or the 
+// enable pin collides with any of your boards CS pins
+#define ENABLE_PIN 8
 
 
 #define TIMER_CLOCK_FREQ 2000000.0 //2MHz for /8 prescale from 16MHz
@@ -43,9 +46,11 @@ char running;
 
 void setup() {
   //configure the enable pin
+  #ifdef ENABLE_PIN
   pinMode(ENABLE_PIN, OUTPUT);
   digitalWrite(ENABLE_PIN,HIGH);
-
+  #endif
+  // SPI.setClockDivider(SPI_CLOCK_DIV128);
   startSerial();
   startMotor();
   //set this according to you stepper
